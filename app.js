c89a047c5c55cb88f47864fd6ef0c3e03011bef1
nilesh dabhi <nileshdabhi1212@gmail.com>
2019-05-28 12:54:48 +0530

    first commint

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var http = require("http");
+var http = require('http');
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(5000);
+var server = http.createServer(function(request, response) {
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:5000/');
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+var port = 3000;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
